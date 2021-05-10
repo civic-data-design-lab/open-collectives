89216bec7b06994f6514f646d382c75eda0778f0
Kwesi Afrifa <kwesiafrifa@gmail.com>
2021-05-10 16:01:56 -0400

    changed submit "/survey" functionality

--- utils.py
+++ utils.py
@@ -19,8 +19,8 @@ def convert_to_array(string):
 def all_false(iterable):
 
     for i in iterable[1:]:
-        if i is None or i == '{}':
-            return False
+        if i is not None and i != '{}':
+            return True
 
-    return True
+    return False
 
